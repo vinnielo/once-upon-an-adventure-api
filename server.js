993671a84d2870a125256d4ec587d1f4bb366a6e
Vinnie Lopez <vinnielo01@gmail.com>
2026-04-30 13:18:23 -0700

    updating cors

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.use(cors());
-
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
